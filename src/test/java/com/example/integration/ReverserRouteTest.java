7409e26ceebf77f5726c8f7656555563a2ef78ee
DerEros <DerEros@users.noreply.github.com>
2018-05-11 22:55:38 +0200

    Auto configure WebTestClient

--- src/test/java/com/example/integration/ReverserRouteTest.java
+++ src/test/java/com/example/integration/ReverserRouteTest.java
@@ -1,6 +1,5 @@
 package com.example.integration;
 
-import com.example.ReverserRoutes;
 import com.example.ReverserService;
 import com.example.model.ReverserResult;
 import com.fasterxml.jackson.databind.Module;
@@ -8,28 +7,29 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureWebTestClient
 public class ReverserRouteTest {
-    @Autowired
-    public ReverserRoutes reverserRoutes;
-
     @MockBean
     public ReverserService mockReverserService;
 
     @Autowired
     @Qualifier("specificSerializers")
     public Module jacksonModule;
+
+    @Autowired
+    public WebTestClient client;
 
     @Test
     public void testSerializerBeanIsPresent() {
@@ -40,7 +40,6 @@ public class ReverserRouteTest {
     public void testRouteAcceptsCall() {
         given(mockReverserService.reverse(anyString())).willReturn(new ReverserResult("foo", "bar"));
 
-        WebTestClient client = WebTestClient.bindToRouterFunction(reverserRoutes.createRouterFunction()).build();
         client.get().uri("/reverse/FooBar").exchange().expectStatus().isOk();
     }
 
@@ -48,7 +47,6 @@ public class ReverserRouteTest {
     public void testRouteReturnsMockedResult() {
         given(mockReverserService.reverse(anyString())).willReturn(new ReverserResult("foo", "bar"));
 
-        WebTestClient client = WebTestClient.bindToRouterFunction(reverserRoutes.createRouterFunction()).build();
         client.get().uri("/reverse/somethingcompletelydifferent")
                 .exchange()
                 .expectBody().json("{\"result\":\"foo|bar\"}");
